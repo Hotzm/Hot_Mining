5496775038bacc9b3dd52f47f47a654fe3f2a6b2
Hotzm Dev <124932470+Hotzm@users.noreply.github.com>
2024-04-06 13:47:45 +0200

    Add files via upload

---
+++ Ht_mining/fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Hotzm'
+description 'Hot miner system'
+version '1.1.1'
+
+client_scripts {
+    'bridge/client.lua',
+    'client/*.lua',
+}
+
+server_scripts {
+    'bridge/server.lua',
+    'server/*.lua',
+}
+
+shared_scripts {
+    'config.lua',
+    '@ox_lib/init.lua'
+}
